6c0c28e5fec8c360a7fb208d379e02317a3d2f69
gdmec07150725 <715296475@qq.com>
2018-06-09 17:31:28 +0800

    Version3

--- server/routes/indexs.js
+++ server/routes/indexs.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page.*/
-router.get('/',checkLogind);
+/*router.get('/',checkLogind);*/
 router.get('/', function(req, res, next) {
  /* req.flash('success','登录成功');*/
   res.render('indexs', { title: 'wocean后台管理系统' });
